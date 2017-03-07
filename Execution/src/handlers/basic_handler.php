320f7224ccddfdcc75d87ff2d1bc52a86c9e6d25
bmdevel <developers@billiger-mietwagen.de>
2017-03-07 17:44:04 +0100

    Update basic_handler.php

--- Execution/src/handlers/basic_handler.php
+++ Execution/src/handlers/basic_handler.php
@@ -30,11 +30,11 @@ class ezcExecutionBasicErrorHandler implements ezcExecutionErrorHandler
      * should write your own class that implements the ezcExecutionErrorHandler
      * interface and use that as parameter to {@link ezcExecution::init()}
      *
-     * @param Exception $e
+     * @param Exception|Throwable $e
      *
      * @return void
      */
-    static public function onError( Exception $e = null )
+    static public function onError( $e = null )
     {
         echo <<<END
 This application stopped in an unclean way. Please contact the site
